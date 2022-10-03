108d689293e1f393feca5bd85e0777929224e30e
shaxikh2007 <shaxikhudoynazarov@gmail.com>
2022-10-03 16:48:42 +0300

    first commit

---
+++ porfolio new/index.js
@@ -0,0 +1,17 @@
+
+
+
+
+let year = document.querySelector(".year");
+
+let birthday = data.getFullYear();
+
+let brithyear = 2007;
+
+let result = birthday - brithyear;
+
+year.innerHtml = result;
+
+console.log(result);
+
+
